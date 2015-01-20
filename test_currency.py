3126c09022c7eccd86f3af8f7b91741b28eebaf7
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-01-20 13:50:36 -0500

    Tested to make sure it converts 1 USD to 0.74 EUR.

--- test_currency.py
+++ test_currency.py
@@ -6,4 +6,5 @@ USD_EUR = ("USD", "EUR", 0.74)
 EUR_JPY = ("EUR", "JPY", 145.949)
 
 def test_converter():
+    assert convert(USD_EUR, 1, "USD", "EUR") == .74
     assert convert(USD_EUR, 1, "USD", "USD") == 1
