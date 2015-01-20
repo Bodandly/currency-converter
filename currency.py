7751fc9a9ff0be30ebe87e8c18a37025e8fb7438
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-01-20 14:25:28 -0500

    Didn't realize that it was a list of tuples... Added a new function to find the correct tuple, and then refactored convert to work correctly.

--- currency.py
+++ currency.py
@@ -1,7 +1,18 @@
+
+rate_list =  [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
+
 def convert(rates, value, begin, to):
-    if rates.index(begin) == rates.index(to):
+    rate = rate_finder(rates, begin, to)
+    if rate == False:
         return (1 * value)
-    elif rates.index(begin) == 1:
-        return round(value * (1/rates[2]), 2)
+    elif rate.index(begin) == 1:
+        return round(value * (1/rate[2]), 2)
     else:
-        return round(value * rates[2], 2)
+        return round(value * rate[2], 2)
+
+def rate_finder(rates, begin, to):
+    if begin == to:
+        return False
+    for entry in rates:
+        if begin in entry and to in entry:
+            return entry
--- test_currency.py
+++ test_currency.py
@@ -3,10 +3,11 @@ from currency import *
 
 rate_list =  [("USD", "EUR", 0.74), ("EUR", "JPY", 145.949)]
 
-def xtest_converter():
+def test_converter():
     assert convert(rate_list, 1, "USD", "EUR") == 0.74
     assert convert(rate_list, 1, "USD", "USD") == 1
     assert convert(rate_list, 1, "EUR", "USD") == 1.35
 
 def test_rate_finder():
-    assert rate_finder(rate_list, begin, to) == ("USD", "EUR", 0.74)
+    assert rate_finder(rate_list, "USD", "USD") == False
+    assert rate_finder(rate_list, "USD", "EUR") == ("USD", "EUR", 0.74)
