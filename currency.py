eae13a2434271d99908ac33683416c07560c5f3b
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-01-20 13:54:46 -0500

    Got the test converting from EUR to USD working.

--- currency.py
+++ currency.py
@@ -2,6 +2,6 @@ def convert(rates, value, begin, to):
     if rates.index(begin) == rates.index(to):
         return (1 * value)
     elif rates.index(begin) == 1:
-        return value * (1/rates[2])
+        return round(value * (1/rates[2]), 2)
     else:
-        return value * rates[2]
+        return round(value * rates[2], 2)
--- test_currency.py
+++ test_currency.py
@@ -6,5 +6,6 @@ USD_EUR = ("USD", "EUR", 0.74)
 EUR_JPY = ("EUR", "JPY", 145.949)
 
 def test_converter():
-    assert convert(USD_EUR, 1, "USD", "EUR") == .74
+    assert convert(USD_EUR, 1, "USD", "EUR") == 0.74
     assert convert(USD_EUR, 1, "USD", "USD") == 1
+    assert convert(USD_EUR, 1, "EUR", "USD") == 1.35
